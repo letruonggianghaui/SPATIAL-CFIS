e06f96eabe8ffce9de1c6b4d750aca5a0c431d4b
Snow <luongxuongcho@gmail.com>
2021-02-22 14:26:35 +0700

    upload source code

---
+++ SplitData.py
@@ -0,0 +1,26 @@
+import os
+
+def validImg(fileName):
+    validFile=['jpg','png']
+    if(fileName[-3:] in validFile):
+        return True
+    return False
+
+def splitImg(folder):
+    cnt = 0
+    numFile = len(os.listdir(folder)) - 1
+    check = True
+    for fileName in sorted(os.listdir(folder)):
+        if(validImg(fileName) and check):
+            cnt+=1
+            img = cv2.imread(os.path.join(folder,fileName))
+            os.rename(os.path.join(folder,fileName), os.path.join('Data',os.path.join('Training Data',fileName)))
+            cv2.imwrite(os.path.join(folder,fileName),img)
+        if (not check):
+            os.rename(os.path.join(folder,fileName), os.path.join('Data',os.path.join('Testing',fileName)))
+            cv2.imwrite(os.path.join(folder,fileName),img)
+        if(cnt>=0.7*numFile):
+            check = False
+if __name__ == "__main__":
+    folder = 'All Data'
+    splitImg(folder)
